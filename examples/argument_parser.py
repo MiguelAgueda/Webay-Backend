dd9acd09153600b63cbf01904f30f4ea9cc619d1
Miguel <40192229+MiguelAgueda@users.noreply.github.com>
2020-02-12 14:36:14 -0800

    Argument Parser Example
    Provided an example of  collecting command line arguments.

---
+++ examples/argument_parser.py
@@ -0,0 +1,27 @@
+"""
+This program accepts various commandline arguments and prints the data to 
+the user. This can help understand how to handle argument calls for functions.
+"""
+
+import argparse
+
+def add_true():
+    print("TRUE!")
+
+parser = argparse.ArgumentParser()  # Create AP object.
+# Below, the parser is used to create a set of valid arguments.
+parser.add_argument('--search', type=str, help="Search Term")
+parser.add_argument('--sort', type=str, help="Sort method")
+parser.add_argument('--create', action='store_true', help="Create new listing.")
+parser.add_argument('--edit', action='store_true', help="Edit existing listing.")
+parser.add_argument('--modify', action='store_true', help="Modify existing listing.")
+parser.add_argument('--delete', action='store_true', help="Delete existing listing.")
+parser.add_argument('--print', action='store_true', help="Print first 50 listings.")
+
+argument_list = parser.parse_args()  # Parse command line arguments.
+
+for key in vars(argument_list):  # Key refers to item being detailed by user.
+    value = getattr(argument_list, key)  # Value of item, this is users input.
+    if value:  # If the user detailed the value for the item,
+        print(key + ": " + str(value))
+
