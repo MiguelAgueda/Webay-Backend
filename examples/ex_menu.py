ef8a67084bef827fed4dab58e76d60048cfbf13f
anthonysilva98 <47488860+anthonysilva98@users.noreply.github.com>
2020-02-26 08:42:37 -0800

    Update ex_menu.py

--- examples/ex_menu.py
+++ examples/ex_menu.py
@@ -1,3 +1,10 @@
+   
+import json
+import random
+
+
+file_path = "data_files/sample_listings.json"  # Path to data file.
+
 def Menu():
     print("Welcome to Webay. Below are your current options: \n")
     print("1. Browse Webstore:\n")
@@ -21,12 +28,39 @@ def Menu():
         Menu()
 
 def Browse_WebStore():
-    print("Browse webstore")
+    with open(file_path, 'r') as file:  # Open file in read-only mode.
+        data = json.load(file)  # Load data from file.
+   #print(data)  # Print data to the screen.
+        search_sku = input("Please enter in a SKU, item name, or price.(If searching by price please enter in a preceeding $).\n")
+        ("To return to the main menu enter zero.\n")
+        print(data[int (search_sku)].values())
+        Menu()
 
 def Search_Forums():
     print("Search Forums")
 
 def Review_Pages():
     print("Review Pages")
+
+def Quick_Sort(data):
+    data_size = len(data)
+    
+    if(data_size <= 1):
+        return data
+    
+    else:
+ #       piv = data[-1].value(price)
+
+    #numbers_bigger = []
+    #numbers_smaller = []
+
+        for items in data:
+            print (items)
+        #if item > piv:
+           # numbers_bigger.append(items)
+       # else:
+           #numbers_smaller.append(items)
+    
+    #return Quick_Sort(numbers_bigger) + (piv) + Quick_Sort(numbers_bigger)
 
 Menu()
