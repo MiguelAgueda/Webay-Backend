b520608e07a7ea415756bb8b707dcb8f7f83188e
anthonysilva98 <47488860+anthonysilva98@users.noreply.github.com>
2020-02-26 17:14:07 -0800

    New Search Algoritm
    We were able to create a sucessful search algorithm that allowed us to seach for items based of of their prices from lowest to highest

--- examples/ex_menu.py
+++ examples/ex_menu.py
@@ -1,9 +1,10 @@
-   
+
 import json
 import random
 
 
 file_path = "data_files/sample_listings.json"  # Path to data file.
+
 
 def Menu():
     print("Welcome to Webay. Below are your current options: \n")
@@ -27,40 +28,54 @@ def Menu():
         print("Invalid Input")
         Menu()
 
+
 def Browse_WebStore():
     with open(file_path, 'r') as file:  # Open file in read-only mode.
         data = json.load(file)  # Load data from file.
-   #print(data)  # Print data to the screen.
-        search_sku = input("Please enter in a SKU, item name, or price.(If searching by price please enter in a preceeding $).\n")
+   # print(data)  # Print data to the screen.
+        search_price = input(
+            "Please enter in a SKU, item name, or price.(If searching by price please enter in a preceeding $).\n")
         ("To return to the main menu enter zero.\n")
-        print(data[int (search_sku)].values())
-        Menu()
+        #print(data[int(search_sku)].values())
+        #print(Quick_Sort(data))
+        save_to_file("data_files/price_sorted_listings.json", Quick_Sort(data))
+        #Men
+        #u()troubleshoot
+
 
 def Search_Forums():
     print("Search Forums")
 
+
 def Review_Pages():
     print("Review Pages")
 
-def Quick_Sort(data):
-    data_size = len(data)
-    
+
+def Quick_Sort(listings):
+    data_size = len(listings)
+
     if(data_size <= 1):
-        return data
-    
+        return listings
+
     else:
- #       piv = data[-1].value(price)
+        piv_listing = listings.pop()
+        piv = piv_listing[2]
+        numbers_bigger = []
+        numbers_smaller = []
+        sorted_list = []
 
-    #numbers_bigger = []
-    #numbers_smaller = []
+        for listing in listings:
 
-        for items in data:
-            print (items)
-        #if item > piv:
-           # numbers_bigger.append(items)
-       # else:
-           #numbers_smaller.append(items)
-    
-    #return Quick_Sort(numbers_bigger) + (piv) + Quick_Sort(numbers_bigger)
+            if listing[2] > piv:
+                numbers_bigger.append(listing)
+            else:
+                numbers_smaller.append(listing)
 
+        sorted_list = Quick_Sort(numbers_smaller) + [piv_listing]+ Quick_Sort(numbers_bigger)
+    return sorted_list
+
+def save_to_file(pathtofile, datatosave):
+    with open(pathtofile, 'w' ) as file :
+         json.dump(datatosave, file, separators=( ' , ', ' ] '), indent=2)
+         file.close()  # Close file, writing changes.
 Menu()
