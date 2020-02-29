567538f7cbf72c9eb87613705418bac0c1c114e3
Miguel Agueda <40192229+MiguelAgueda@users.noreply.github.com>
2020-02-28 17:48:51 -0800

    Publicized Private Functions

--- webay_backend/listing_editor.py
+++ webay_backend/listing_editor.py
@@ -22,38 +22,38 @@ class ListingEditor:
     def add_listing(self, path_to_file, title, price):
         """Add listing to data stored on specified path."""
         # Get all currently posted listings.
-        active_listings = self.__safe_read(path_to_file)
+        active_listings = self.safe_read(path_to_file)
         last_sku = active_listings[-1][0]  # Get sku of last listing.
         # Create a safe copy of active listings to write to.
         # Add new listing to end of list.
         active_listings.append([last_sku + 1, title, price])
         # Save listings to file.
-        self.__safe_write(path_to_file, active_listings)
+        self.safe_write(path_to_file, active_listings)
 
     def modify_listing(self, path_to_file, target_sku, title, price):
         """Edit specified listing stored on specified path."""
-        active_listings = self.__safe_read(path_to_file)
+        active_listings = self.safe_read(path_to_file)
         for listing in active_listings:
             if listing[0] == target_sku:  # If listing to edit is found.
                 listing[1] = title  # Edit title.
                 listing[2] = price  # Edit price.
                 break
 
-        self.__safe_write(path_to_file, active_listings)
+        self.safe_write(path_to_file, active_listings)
 
     def delete_listing(self, path_to_file, target_sku):
         """Delete an existing listing with target SKU on specified file."""
-        active_listings = self.__safe_read(path_to_file)
+        active_listings = self.safe_read(path_to_file)
         for index, listing in enumerate(active_listings):
             # If the listing to delete is found.
             if listing[0] == target_sku:
                 del active_listings[index]  # Delete the listing.
                 break
 
-        self.__safe_write(path_to_file, active_listings)
+        self.safe_write(path_to_file, active_listings)
 
     def print_listings(self, path_to_file):
         """Format and print all listings from a specified file."""
-        active_listings = self.__safe_read(path_to_file)
+        active_listings = self.safe_read(path_to_file)
         for listing in active_listings:
             print(F"\nTitle:\t{listing[1]}\nPrice:\t{listing[2]}\n\n")
